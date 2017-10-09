1352dca226a2f9576f65c139e246805bdc0de537
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-10-09 20:31:55 +0900

    feat(bitbank): add methods return type implicitly

--- src/bitbank/bitbank.ts
+++ src/bitbank/bitbank.ts
@@ -1,13 +1,24 @@
-import { BitbankApiCandlestickType } from './api-response.type';
-import { BitbankPublicApiHandler } from './public-api-handler/public-api-handler';
-import { BitbankPubnubHandler } from './pubnub-handler/pubnub-handler';
+import { Observable } from 'rxjs/Observable';
+import { BitbankPublicApiHandler } from '../public-api-handler/public-api-handler';
+import { BitbankPubnubHandler } from '../pubnub-handler/pubnub-handler';
+import {
+  BitbankApiCandlestick,
+  BitbankApiCandlestickType,
+  BitbankApiDepth,
+  BitbankApiMultiOrdersResult,
+  BitbankApiOrder,
+  BitbankApiTicker,
+  BitbankApiTransactions,
+  BitbankApiWithdrawalRequestResult,
+  BitbankApiWithdrawalResult,
+} from '../api-response.type';
 import {
   BitbankApiActiveOrdersOptions,
   BitbankApiCreateWithdrawalRequestOptions,
   BitbankApiOrderOptions,
   BitbankPrivateApiHandler,
   BitbankPrivateApiHandlerOptions,
-} from './private-api-handler/private-api-handler';
+} from '../private-api-handler/private-api-handler';
 
 
 /**
@@ -29,105 +40,105 @@ export class Bitbank {
   /**
    * GET: /{pair}/ticker
    */
-  getTicker(pair: string) {
+  getTicker(pair: string): Observable<BitbankApiTicker> {
     return this.publicApi.getTicker(pair);
   }
 
   /**
    * GET: /{pair}/depth
    */
-  getDepth(pair: string) {
+  getDepth(pair: string): Observable<BitbankApiDepth> {
     return this.publicApi.getDepth(pair);
   }
 
   /**
    * GET: /{pair}/transactions
    */
-  getTransactions(pair: string, yyyymmdd?: string) {
+  getTransactions(pair: string, yyyymmdd?: string): Observable<BitbankApiTransactions> {
     return this.publicApi.getTransactions(pair, yyyymmdd);
   }
 
   /**
    * GET: /{pair}/candlestick
    */
-  getCandlestick(pair: string, type: BitbankApiCandlestickType, yyyymmdd: string) {
+  getCandlestick(pair: string, type: BitbankApiCandlestickType, yyyymmdd: string): Observable<BitbankApiCandlestick> {
     return this.publicApi.getCandlestick(pair, type, yyyymmdd);
   }
 
   /**
    * Get observable of ticker data published by pubnub.
    */
-  getTicker$(pair: string) {
+  getTicker$(pair: string): Observable<BitbankApiTicker> {
     return this.pubnubHandler.getTicker$(pair);
   }
 
   /**
    * Get observable of ticker data published by pubnub.
    */
-  getDepth$(pair: string) {
+  getDepth$(pair: string): Observable<BitbankApiDepth> {
     return this.pubnubHandler.getDepth$(pair);
   }
 
   /**
    * Get observable of transactions data published by pubnub.
    */
-  getTransactions$(pair: string) {
+  getTransactions$(pair: string): Observable<BitbankApiTransactions> {
     return this.pubnubHandler.getTransactions$(pair);
   }
 
   /**
    * Get observable of candlestick data published by pubnub.
    */
-  getCandlestick$(pair: string) {
+  getCandlestick$(pair: string): Observable<BitbankApiCandlestick> {
     return this.pubnubHandler.getCandlestick$(pair);
   }
 
   /**
    * POST: /v1/user/spot/order
    */
-  createOrder(options: BitbankApiOrderOptions) {
+  createOrder(options: BitbankApiOrderOptions): Observable<BitbankApiOrder> {
     return this.privateApi.createOrder(options);
   }
 
   /**
    * POST: /v1/user/spot/cancel_order
    */
-  cancelOrder(pair: string, order_id: string) {
+  cancelOrder(pair: string, order_id: string): Observable<BitbankApiOrder> {
     return this.privateApi.cancelOrder(pair, order_id);
   }
 
   /**
    * POST: /v1/user/spot/cancel_orders
    */
-  cancelOrders(pair: string, order_ids: string[]|number[]) {
+  cancelOrders(pair: string, order_ids: string[]|number[]): Observable<BitbankApiMultiOrdersResult> {
     return this.privateApi.cancelOrders(pair, order_ids);
   }
 
   /**
    * GET: /v1/user/spot/order
    */
-  getOrder(pair: string, order_id: string) {
+  getOrder(pair: string, order_id: string): Observable<BitbankApiOrder> {
     return this.privateApi.getOrder(pair, order_id);
   }
 
   /**
    * POST: /v1/user/spot/orders_info
    */
-  getOrders(pair: string, order_ids: string[]) {
+  getOrders(pair: string, order_ids: string[]): Observable<BitbankApiMultiOrdersResult> {
     return this.privateApi.getOrders(pair, order_ids);
   }
 
   /**
    * GET: /v1/user/spot/active_orders
    */
-  getActiveOrders(pair: string, options?: BitbankApiActiveOrdersOptions) {
+  getActiveOrders(pair: string, options?: BitbankApiActiveOrdersOptions): Observable<BitbankApiMultiOrdersResult> {
     return this.privateApi.getActiveOrders(pair, options);
   }
 
   /**
    * GET: /v1/user/withdrawal_account
    */
-  getWithdrawAccount(asset: string) {
+  getWithdrawAccount(asset: string): Observable<BitbankApiWithdrawalResult> {
     return this.privateApi.getWithdrawAccount(asset);
   }
 
@@ -136,7 +147,7 @@ export class Bitbank {
    *
    * @see - https://docs.bitbank.cc/#!/Withdraw/request_withdrawal
    */
-  createWithdrawalRequest(options: BitbankApiCreateWithdrawalRequestOptions) {
+  createWithdrawalRequest(options: BitbankApiCreateWithdrawalRequestOptions): Observable<BitbankApiWithdrawalRequestResult> {
     return this.privateApi.createWithdrawalRequest(options);
   }
 }
